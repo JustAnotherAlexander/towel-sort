e1578c547745f46f48f2b6b3d8a3d4f257198441
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-09 20:42:58 +0300

    feat: add function to sort the input matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result =[];
+  let row_counter=0;
+  
+  if (matrix==undefined || matrix[0]==undefined) {
+    return result;
+  }
+
+  console.log('matrix[i].length: '+matrix[0].length);
+  if (matrix.length>0) {
+    for (let i=0; i<matrix.length; i++) {
+      if (row_counter==0){
+        for (let j=0; j<matrix[i].length; j++) {
+          result.push(matrix[i][j]);
+        }
+        row_counter=1;
+        continue;
+      }  else {
+        for (let j=matrix[i].length-1; j>=0; j--) {
+          result.push(matrix[i][j]);
+        }
+        row_counter=0;
+        continue;
+      }
+    }
+    return result;
+  } else return result;
+  
 }
